1454293d2f4e374033c0534d92fe05c6e9b56e45
huygm1510 <tranconghuyhuy.gm@gmail.com>
2023-05-16 00:04:32 +0700

    commit

--- src/main/java/com/demo/jpa/repository/BookRepository.java
+++ src/main/java/com/demo/jpa/repository/BookRepository.java
@@ -12,12 +12,10 @@ public interface BookRepository extends CrudRepository<BookEntity, Integer> {
 
     List<BookEntity> findByAuthor(String author);
 
-//    @Query("select b from BookEntity b where b.name like ?1%")
-//        //?1 = para 1  ?2 = para 2
-//    List<BookEntity> getBookStartWith(String name);
-
-    @Query(value = "Select b from BookEntity b where b.name like ?1%")
+    @Query("select b from BookEntity b where b.name like ?1%")
     List<BookEntity> getBookStartWith(String name);
+
+
 
     @Query(value = "select * from book where price < ?1 and numberPage >= ?2",nativeQuery = true)
     List<BookEntity> getBookWherePriceLessThanAndNumOfPageGreaterThan(double price, int numberPage);
